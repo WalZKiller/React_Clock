dcbcef164e8e0b090d088c94d763c60d13021e1d
Walid <walidwajdy@gmail.com>
2021-09-05 13:45:32 +0800

    try setInterval direct

--- src/App.js
+++ src/App.js
@@ -9,16 +9,20 @@ import { useClockFetch } from './hooks/useClockFetch';
 function App() {
   //const[state, useState] = useState(currentTime);
 
-  /*const date = new Date();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
-  const second = date.getSeconds();
+  setInterval(() => {
+    const date = new Date();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+  
+    const currentTime = `${hour}:${minute}:${second}`
+      
+  }, 1000)
 
-  const currentTime = `${hour}:${minute}:${second}`*/
-
+  
   return (
     <>
-      <Clock />
+      <Clock time={setInterval}/>
     </>
   );
 }
